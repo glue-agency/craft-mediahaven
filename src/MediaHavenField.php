726269fdbeaa221cca82a6c1e1a97f49bd2cccab
Joren Van Hee <jorenvanhee@gmail.com>
2019-01-07 10:52:04 +0100

    Add empty settings template

--- src/MediaHavenField.php
+++ src/MediaHavenField.php
@@ -3,11 +3,21 @@
 namespace GlueAgency\MediaHaven;
 
 use craft\base\Field;
+use Craft;
 
 class MediaHavenField extends Field
 {
     public static function displayName(): string
     {
         return 'MediaHaven';
+    }
+
+    public function getSettingsHtml()
+    {
+        return Craft::$app->view->renderTemplate(
+            'mediahaven/_settings', [
+                'field' => $this,
+            ]
+        );
     }
 }
