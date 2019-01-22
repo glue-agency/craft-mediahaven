dafd4752fe5cc77bfd7b650d2f1fb1334ae8e771
Joren Van Hee <jorenvanhee@gmail.com>
2019-01-22 10:54:58 +0100

    Make Facet a functional component

--- resources/js/mediaHavenField/Facet.js
+++ resources/js/mediaHavenField/Facet.js
@@ -2,48 +2,39 @@ import React from 'react';
 import FacetCheckbox from './FacetCheckbox';
 import FacetValueFilter from './Filters/FacetValue';
 
-class Facet extends React.Component {
-  constructor(props) {
-    super(props);
+function Facet(props) {
+  const {
+    facet, onAddFacetValue, onRemoveFacetValue, filters
+  } = props;
 
-    this.state = {
-    };
+  const checkboxElements = facet.value
+    .map((value, index) => {
+      const checked = filters
+        .filter(filter => filter instanceof FacetValueFilter)
+        .some(filter => filter.value.atom === value.atom);
+
+      return checked ? null : (
+        <FacetCheckbox
+          key={index}
+          value={value}
+          onAddFacetValue={onAddFacetValue}
+          onRemoveFacetValue={onRemoveFacetValue}
+          checked={checked}
+        />
+      );
+    })
+    .filter(element => element);
+
+  if (!checkboxElements.length) {
+    return null;
   }
 
-  render() {
-    const {
-      facet, onAddFacetValue, onRemoveFacetValue, filters
-    } = this.props;
-
-    const checkboxElements = facet.value
-      .map((value, index) => {
-        const checked = filters
-          .filter(filter => filter instanceof FacetValueFilter)
-          .some(filter => filter.value.atom === value.atom);
-
-        return checked ? null : (
-          <FacetCheckbox
-            key={index}
-            value={value}
-            onAddFacetValue={onAddFacetValue}
-            onRemoveFacetValue={onRemoveFacetValue}
-            checked={checked}
-          />
-        );
-      })
-      .filter(element => element);
-
-    if (!checkboxElements.length) {
-      return null;
-    }
-
-    return (
-      <div>
-        <div className="heading"><span>{facet.title}</span></div>
-        {checkboxElements}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <div className="heading"><span>{facet.title}</span></div>
+      {checkboxElements}
+    </div>
+  );
 }
 
 export default Facet;
